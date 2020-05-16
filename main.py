6c32b8f2261bdac4590f63720a52fce1fddf8d68
LEEjaeHyun <swaa23@gmail.com>
2020-05-16 23:03:59 +0900

    POST 요청 후 값 처리

--- main.py
+++ main.py
@@ -19,19 +19,18 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     print(render_template('index.html'))
+
     return render_template('index.html')
 
-
+## Post 요청 
 @app.route("/post", methods=['POST'])
 def post():
     db = RestfulModelling()
     url = request.get_json()
-
+    # 조회해서 데이터가 비어있으면 포스트 요청을 한 후 다시 값을 받아옴
     if db.getWork(url) == 'None':
-        print('LogPoint4')
         db.postWork(url)
         r = db.getWork(url)
-
     else:
         r = db.getWork(url)
 
